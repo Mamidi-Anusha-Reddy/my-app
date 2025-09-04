cfdc3390056fb50a28f1c5c561c9b144705d5cda
Mamidi-Anusha-Reddy <mamidianusha333@gmail.com>
2025-09-04 16:57:15 +0530

    Update ApplicationService.js

--- src/services/ApplicationService.js
+++ src/services/ApplicationService.js
@@ -1,18 +1,13 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api/applications"; // backend url
+const API_BASE_URL = "http://localhost:8080/api/applications"; // backend endpoint
 
 // Submit application with files
 export const submitApplication = (formData) => {
   const data = new FormData();
-  data.append("fullName", formData.fullName);
-  data.append("phoneNumber", formData.phoneNumber);
-  data.append("email", formData.email);
-  data.append("creditCardType", formData.creditCardType);
-  data.append("profileType", formData.profileType);
-  data.append("idProof", formData.idProof);
-  data.append("addressProof", formData.addressProof);
-  data.append("incomeProof", formData.incomeProof);
+  Object.keys(formData).forEach((key) => {
+    data.append(key, formData[key]);
+  });
 
   return axios.post(API_BASE_URL, data, {
     headers: { "Content-Type": "multipart/form-data" },
@@ -22,6 +17,6 @@ export const submitApplication = (formData) => {
 // Get application details by ID
 export const getApplication = (id) => axios.get(`${API_BASE_URL}/${id}`);
 
-// Get document file by type
+// Get specific document
 export const getDocument = (id, docType) =>
   axios.get(`${API_BASE_URL}/${id}/documents/${docType}`, { responseType: "blob" });
