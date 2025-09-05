65e507c699442fca0d9f0b9c0b6fc9e665bed67e
Mamidi-Anusha-Reddy <mamidianusha333@gmail.com>
2025-09-05 12:33:28 +0530

    Update ApplicationService.js

--- src/services/ApplicationService.js
+++ src/services/ApplicationService.js
@@ -1,15 +1,41 @@
-// src/services/applicationService.js
-import axios from "axios";
+import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:8080";
+const api = axios.create({
+  baseURL: 'http://localhost:8080/api',
+  timeout: 30000
+});
 
-export async function submitApplicationFormData(formData) {
-  // POST multipart/form-data to backend
-  const url = `${API_BASE}/api/applications`;
-  const config = {
-    headers: { "Content-Type": "multipart/form-data" },
-    // withCredentials: false, // optional
-  };
-  const res = await axios.post(url, formData, config);
-  return res.data;
-}
+export default api;
+
+
+import api from './api';
+
+// Submit credit card application
+export const submitApplication = async (applicationData, files) => {
+  const formData = new FormData();
+  
+  formData.append('application', JSON.stringify(applicationData));
+  formData.append('idProof', files.idProof);
+  formData.append('addressProof', files.addressProof);
+  formData.append('incomeProof', files.incomeProof);
+  
+  const response = await api.post('/applications', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  
+  return response.data;
+};
+
+// Get all applications
+export const getAllApplications = async () => {
+  const response = await api.get('/applications');
+  return response.data;
+};
+
+// Get application by ID
+export const getApplicationById = async (id) => {
+  const response = await api.get(`/applications/${id}`);
+  return response.data;
+};
